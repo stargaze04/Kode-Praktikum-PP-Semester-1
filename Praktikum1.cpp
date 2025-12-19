ec9d93ed4e779418c7b8de149e2098b26d962e79
Bibiii <rahmatbintang20an@gmail.com>
2025-12-19 17:43:15 +0700

    Add files via upload

---
+++ Praktikum1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+string nama = "Wenno";
+string NPM = "2517051039";
+
+cout << "Selamat Datang Di Praktikum Pengenalan Pemrograman" << endl;
+cout << "Nama saya adalah " << nama << endl;
+cout << "NPM saya adalah " << NPM << endl << endl;
+cout << "Unila Indah" << endl;
+cout << "Be Strong" << endl;
+}
